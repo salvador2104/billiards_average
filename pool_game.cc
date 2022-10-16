e6c21565a007d5e70ac3beeef987cb62eaa1504d
salvador2104 <112212072+salvador2104@users.noreply.github.com>
2022-10-16 14:26:44 -0700

    Update pool_game.cc

--- pool_game.cc
+++ pool_game.cc
@@ -5,34 +5,17 @@
 
 using namespace std;
 // create a program that displays a scoreboard and a shot average for a player in 8 and 9 ball
-
-int main() {
-  //ask the user to pick from a gamemode
-  int game_mode {};
-  cout << "\n\nWould you like to play [9]Ball or [10]Ball : ";
-  cin >> game_mode;
-  //ask the user to say wether he made or missed the ball until the game is over
-  bool game_over = true;
-  int shots_taken {};
-  int shots_made {};
-  bool shot_made {};
-  while (game_over) {
-    cout << "\n\nHit[1] or Miss[2] : ";
-    cin >> shot_made;
-    if (shot_made) {
-      shots_taken += 1;
-      shots_made += 1;
-    } else {
-      shots_taken += 1;
-    }
-    if (shots_taken == game_mode) {
-      game_over = false;
-    }
+int takeShot() { // asked player if the shot hit or missed then return true if hit and false if miss
+  cout << "Hit [1] or Miss [0] : ";
+  bool shot_result {};
+  cin >> shot_result;
+  while ((shot_result != true) &&
+         (shot_result != false)) {
+    cout << "Not a choice, Try Again.";
+    cin >> shot_result;
   }
-  //tell the user the stats of the game when they are finished
-  int matches {};
-  double average {};
-  average = shots_made / shots_taken;
-  cout << "In " << matches << " matches your shot average was " << average << endl;
-  //ask the user if he wants to play another game
+  return shot_result;
+}
+int main() {
+  return 0;
 }
