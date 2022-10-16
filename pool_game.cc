15a5e46025f4308758cee9e87f1810d1caab73b3
salvador2104 <112212072+salvador2104@users.noreply.github.com>
2022-10-15 17:05:47 -0700

    changed the source code

--- pool_game.cc
+++ pool_game.cc
@@ -4,61 +4,35 @@
 #include <string>
 
 using namespace std;
-void indent() {
-  cout << endl;
-}
-void printDivider (){
-  cout << "     |     ";
-}
-void formatGame(int game_mode) {
-  printDivider();
-  cout << "Gamemode [" << game_mode << " ball]";
-  printDivider();
-  indent();
-  cout << "Shot";
-  printDivider();
-  cout << "Hit [1] or Miss [0]?";
-  printDivider();
-  cout << "Average";
-  indent();
-}
-int takeShot(int shot_number) {
-  cout << "Shot " << shot_number;
-  printDivider();
-  bool shot_result {};
-  cin >> shot_result;
-  int shots_made {};
-  int shots_taken {};
-  if (shot_result == true){
-    shots_made += 1;
-    shots_taken += 1;
-  } else {
-    shots_taken += 1;
-  }
-  printDivider();
-  double average {};
-  average = (shots_made / shots_taken);
-  cout << average;
-  return shots_made;
-}
-int main(int argc, char* argv[]){
-  cout << "Will you be playing [8] ball, [9] ball, or [10] ball";
+// create a program that displays a scoreboard and a shot average for a player in 8 and 9 ball
+
+int main() {
+  //ask the user to pick from a gamemode
   int game_mode {};
+  cout << "\n\nWould you like to play [9]Ball or [10]Ball : ";
   cin >> game_mode;
-  if (game_mode == 8){
-    formatGame(game_mode);
-    bool keep_playing {true};
-    while(keep_playing) {
-      int shot_number {1};
-      int shots_made {};
-      shots_made = takeShot(shot_number);
-      shot_number += 1;
-      
-      if (shots_made >= 17) {
-        keep_playing = false;
-        return 0;
-      }
+  //ask the user to say wether he made or missed the ball until the game is over
+  bool game_over = true;
+  int shots_taken {};
+  int shots_made {};
+  bool shot_made {};
+  while (game_over) {
+    cout << "\n\nHit[1] or Miss[2] : ";
+    cin >> shot_made;
+    if (shot_made) {
+      shots_taken += 1;
+      shots_made += 1;
+    } else {
+      shots_taken += 1;
+    }
+    if (shots_taken == game_mode) {
+      game_over = false;
     }
   }
-  return 0;
+  //tell the user the stats of the game when they are finished
+  int matches {};
+  double average {};
+  average = shots_made / shots_taken;
+  cout << "In " << matches << " matches your shot average was " << average << endl;
+  //ask the user if he wants to play another game
 }
